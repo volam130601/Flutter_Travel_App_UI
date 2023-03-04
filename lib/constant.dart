695ebb8743173edd8cef6dfc3a9b0f33cb8882d8
volam130601 <volam13062001@gmail.com>
2023-03-04 17:42:06 +0700

    Initial commit

---
+++ lib/constant.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const Color colorOrange = Color(0xFFF19036);
+const Color colorCyan = Color(0xFF35F2E8);
+const Color colorPink = Color(0xFFF85BA6);
+const Color colorGrey = Color(0xFFC8C9D4);
+const Color colorPurple = Color(0xFF9E6FEA);
+
+const Color colorBlue = Color(0xFF38A4F1);
+const Color colorBlueSky = Color(0xFF87A4EF);
+const Color colorBlueDark = Color(0xFF00044D);
+
+const Color bgColor = Color(0xFFFDFDFD);
+
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 12.0;
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_travel_app/screen/landing/landing_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LandingScreen(),
+    );
+  }
+}
+
